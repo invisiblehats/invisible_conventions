d297fd71d9396708ef007e49d9721230cd783707
Myk Klemme <mykklemme@gmail.com>
2019-11-16 23:11:23 -0800

    Fix nested class issue

--- lib/invisible_standards/engine.rb
+++ lib/invisible_standards/engine.rb
@@ -2,21 +2,19 @@
 
 require 'github_changelog_generator/task'
 
-module InvisibleStandards
-  class Engine < ::Rails::Engine
-    rake_tasks do
-      # These will be moved into a different repo soon
-      namespace :invisible_toolkit do
-        namespace :changelog do
-          desc 'Create a new changelog based on the current version'
-          GitHubChangelogGenerator::RakeTask.new :generate do |config|
-            user, project = ENV['CHANGELOG_REPO'].split('/')
+class InvisibleStandards::Engine < ::Rails::Engine
+  rake_tasks do
+    # These will be moved into a different repo soon
+    namespace :invisible_toolkit do
+      namespace :changelog do
+        desc 'Create a new changelog based on the current version'
+        GitHubChangelogGenerator::RakeTask.new :generate do |config|
+          user, project = ENV['CHANGELOG_REPO'].split('/')
 
-            config.user = user
-            config.project = project
-            config.future_release = ENV['CHANGELOG_VERSION']
-            config.token = ENV['CHANGELOG_GITHUB_TOKEN'] || ENV['GITHUB_TOKEN']
-          end
+          config.user = user
+          config.project = project
+          config.future_release = ENV['CHANGELOG_VERSION']
+          config.token = ENV['CHANGELOG_GITHUB_TOKEN'] || ENV['GITHUB_TOKEN']
         end
       end
     end
