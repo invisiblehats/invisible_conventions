a8f5c0b278dea4fb2acb34005a6fd49c678be9ed
Myk Klemme <mykklemme@gmail.com>
2019-11-16 16:12:37 -0800

    Add module to store gem information
    Otherwise we would have a lot of extra files if we follow how VERSION 
    was setup.

---
+++ lib/invisible_standards/project_meta.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module InvisibleStandards
+  VERSION = '0.1.1'
+  NAME = 'invisible_standards'
+  REPO = 'invisiblehats/invisible_standards'
+  AUTHORS = ['Myk Klemme'].freeze
+  EMAIL = ['mykklemme@gmail.com'].freeze
+  HOMEPAGE = 'https://github.com/invisiblehats/invisible_standards'
+  SUMMARY = 'Summary of InvisibleStandards.'
+  DESCRIPTION = 'Description of InvisibleStandards.'
+  LICENSE = 'MIT'
+end
