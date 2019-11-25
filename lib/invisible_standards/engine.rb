f29484eee417a9ef9609d378834951d2fe25c34e
Myk Klemme <mykklemme@gmail.com>
2019-11-25 09:36:15 -0800

    Only build changelog task if changelog env var present

--- lib/invisible_standards/engine.rb
+++ lib/invisible_standards/engine.rb
@@ -7,14 +7,16 @@ class InvisibleStandards::Engine < ::Rails::Engine
     # These will be moved into a different repo soon
     namespace :invisible_toolkit do
       namespace :changelog do
-        desc 'Create a new changelog based on the current version'
-        GitHubChangelogGenerator::RakeTask.new :generate do |config|
-          user, project = ENV['CHANGELOG_REPO'].split('/')
+        if ENV['CHANGELOG_REPO'].present?
+          desc 'Create a new changelog based on the current version'
+          GitHubChangelogGenerator::RakeTask.new :generate do |config|
+            user, project = ENV['CHANGELOG_REPO'].split('/')
 
-          config.user = user
-          config.project = project
-          config.future_release = ENV['CHANGELOG_VERSION']
-          config.token = ENV['CHANGELOG_TOKEN'] || ENV['GITHUB_TOKEN']
+            config.user = user
+            config.project = project
+            config.future_release = ENV['CHANGELOG_VERSION']
+            config.token = ENV['CHANGELOG_TOKEN'] || ENV['GITHUB_TOKEN']
+          end
         end
       end
     end
